50f97637b73c4d9252e4d26f931eed4c462328c6
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-14 23:46:59 +0000

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,18 +15,19 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """intilization function"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """"format function"""
         rec = super(RedactingFormatter, self).format(record)
         mess = filter_datum(self.fields, self.REDACTION,
                             rec, self.SEPARATOR)
