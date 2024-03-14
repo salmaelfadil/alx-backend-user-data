7c9e77ecc84ae046ad79ba12040df52d0b509ab4
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-14 23:28:57 +0000

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,14 +4,10 @@ from typing import List
 import re
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str) -> str:
-    """returns log message"""
-    for field in fields:
-        pattern = f'{field}=.*?{separator}'
-        replacement = f"{field}={redaction}{separator}"
-        message = (pattern, replacement, message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """hides sensitive data"""
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
