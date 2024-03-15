f96e3fd381e970c7c94e8888a61e1a4f2e3a7513
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-15 00:31:52 +0000

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import os
 import mysql.connector
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -55,9 +55,9 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """gets the database to connect to"""
 
-    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME") or "root"
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD") or ""
+    host = os.getenv("PERSONAL_DATA_DB_HOST") or "localhost"
     database = os.getenv("PERSONAL_DATA_DB_NAME")
 
     conn = mysql.connector.connect(user=user, password=password,
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -3,10 +3,16 @@
 Main file
 """
 
-import logging
+get_db = __import__('filtered_logger').get_db
 
-get_logger = __import__('filtered_logger').get_logger
-PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close()
 
-print(get_logger.__annotations__.get('return'))
-print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+bob@dylan:~$
+bob@dylan:~$ PERSONAL_DATA_DB_USERNAME=root PERSONAL_DATA_DB_PASSWORD=root PERSONAL_DATA_DB_HOST=localhost PERSONAL_DATA_DB_NAME=my_db ./main.py
+2
