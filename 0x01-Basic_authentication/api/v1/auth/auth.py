fddc666b124aca7586963fb813d3fc101ea3edaf
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-19 23:09:26 +0000

    .

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,14 @@ class Auth:
     """basic auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth function"""
-        return False
+        if path in excluded_paths:
+            return False
+        return True
+
     def authorization_header(self, request=None) -> str:
         """authorization header function"""
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """returns current user"""
         return None
