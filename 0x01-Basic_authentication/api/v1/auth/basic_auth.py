3100981fefc8cf7f29b81d3d6c5fda89514dd7bc
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-20 00:39:11 +0000

    .

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic Auth Class"""
 from .auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -42,3 +43,22 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_base64_authorization_header.split(":")
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns a user instance"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exeption:
+            return
+
+
