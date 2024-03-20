a00bf6bf8ff2c9a10fdc65493e686b63f4cecfcc
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-20 00:18:36 +0000

    .

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Basic Auth Class"""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic Auth Class"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """returns the Base64 encoding of the header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[-1]
