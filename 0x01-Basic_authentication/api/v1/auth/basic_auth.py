c2b977735e7a7bab39679ec445af328a3e73cd58
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-20 00:41:12 +0000

    .

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,7 +58,7 @@ class BasicAuth(Auth):
             for user in users:
                 if user.is_valid_password(user_pwd):
                     return user
+            return None
         except Exeption:
-            return
-
+            return None
 
