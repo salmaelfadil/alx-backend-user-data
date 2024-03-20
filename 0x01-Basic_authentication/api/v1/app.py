36f67cf5135b39b4e38a1a5b382a6dfc3bcdca25
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-20 00:00:44 +0000

    .

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,7 +45,7 @@ def bef_request():
     ex_list = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
             ]
     if auth.require_auth(request.path, ex_list):
         if auth.authorization_header(request) is None:
