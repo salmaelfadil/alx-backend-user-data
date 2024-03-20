bdd731a31bee7bcff43f6a65535ed537baac8d84
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-20 00:43:49 +0000

    .

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,6 +59,13 @@ class BasicAuth(Auth):
                 if user.is_valid_password(user_pwd):
                     return user
             return None
-        except Exeption:
+        except Exception:
             return None
 
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns a user instance for a request"""
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
