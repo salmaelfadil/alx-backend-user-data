3680dccf64b68a93364ffa289efad6dc75f86d32
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-22 00:19:41 +0000

    .

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,8 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a user based on id"""
+        if session_id is None or isinstance(user_id, str):
+            return None
+        return (self.user_id_by_session_id.get(session_id))
