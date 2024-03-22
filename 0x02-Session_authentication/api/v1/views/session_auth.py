45c2c095d34a864112625a52143069f97dcfa512
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-22 16:07:59 +0000

    .

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,23 +5,24 @@ from api.v1.auth.session_auth import SessionAuth
 from models.user import User
 from api.v1.views import app_views
 from os import getenv
-from typing import Tuple
+from typing import Tuple, Dict
 
 auth = SessionAuth()
+
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> Tuple[str, int]:
     """login method"""
     email = request.form.get('email')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     password = request.form.get('password')
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     try:
         users = User.search({'email': email})
         if not users or users == []:
-            return jsonify({ "error": "no user found for this email" }), 404
+            return jsonify({"error": "no user found for this email"}), 404
         for user in users:
             if user.is_valid_password(password):
                 user_id = user.id
@@ -31,6 +32,16 @@ def login() -> Tuple[str, int]:
                 res.set_cookie(getenv('SESSION_NAME'), session_id)
                 return res
             else:
-                return jsonify({ "error": "wrong password" }), 401
+                return jsonify({"error": "wrong password"}), 401
     except Exception:
         return
+
+
+@app_views.route('/api/v1/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout() -> Dict:
+    """logout method"""
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    else:
+        abort(404)
