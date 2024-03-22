94b19946dbc58719b4a4571746967b85be38f9f3
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-22 14:21:36 +0000

    .

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from Typing import Tuple
 
 
 app = Flask(__name__)
