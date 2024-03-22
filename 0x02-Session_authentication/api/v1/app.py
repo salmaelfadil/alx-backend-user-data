cb58846bb7acecd0d11eebf02367ab39ea79d47d
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-22 01:45:17 +0000

    .

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,9 +52,13 @@ def bef_request():
     ex_list = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
             ]
     if auth.require_auth(request.path, ex_list):
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
+            abort(401)
         if auth.authorization_header(request) is None:
             abort(401, description="Unauthorized")
         if auth.current_user(request) is None:
