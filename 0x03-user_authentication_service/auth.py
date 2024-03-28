eaa722e2da0cd316e081e5cc30ab4b94410e829e
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-28 22:05:19 +0000

    .

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,12 +36,13 @@ class Auth:
             user = self._db.add_user(email, hash)
             return user
 
-    def valid_login(self, email, password) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """valudates login"""
         try:
             user = self._db.find_user_by(email=email)
             if user is not None:
-                return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+                return bcrypt.checkpw(password.encode("utf-8"),
+                                      user.hashed_password)
         except Exception:
             return False
         return False
