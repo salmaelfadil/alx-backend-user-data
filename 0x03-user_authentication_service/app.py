64edbee4fa19dd432bd5450bfb460b2b9216ed06
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-28 21:06:44 +0000

    .

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""flask app"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """route handler for root endpoint"""
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
